54ac547b49a0f8d7b7be0d4a3cd140dd66243147
Justin Lin <jklin1206@gmail.com>
2026-04-20 21:44:24 -0500

    chore: add UNBOUNDTests target

---
+++ UNBOUNDTests/UNBOUNDTests.swift
@@ -0,0 +1,9 @@
+// UNBOUNDTests/UNBOUNDTests.swift
+import XCTest
+@testable import UNBOUND
+
+final class UNBOUNDSmokeTest: XCTestCase {
+    func testSmoke() {
+        XCTAssertTrue(true)
+    }
+}
